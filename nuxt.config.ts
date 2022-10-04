09adac57071ce8be3a6d05b8e0bcbfc0cc6d7b18
Tobias Sindek <tobias.sindek@gmail.com>
2022-10-04 14:02:11 +0200

    feat: add about and blog page. add content to blog

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  modules: [
+    ["@storyblok/nuxt", { accessToken: "2l4vYwCuImc2KkrFPeFRUgtt" }],
+    "@nuxtjs/tailwindcss",
+  ],
+});
---
+++ storyblok/Feature.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    v-editable="blok"
+    class="w-full bg-[#f7f6fd] rounded-[5px] text-center overflow-hidden"
+  >
+    <img
+      :src="optimizedImage"
+      :alt="blok.image.alt"
+      class="w-full h-48 xl:h-72 object-cover"
+    />
+    <div class="px-12 py-6">
+      <h3 class="text-2xl text-[#1d243d] font-bold">{{ blok.name }}</h3>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({ blok: Object });
+
+const optimizedImage = computed(() => {
+  return `${props.blok.image.filename}/m/`;
+});
+</script>
